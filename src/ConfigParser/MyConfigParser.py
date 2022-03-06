0e82b1edd69b3faead743139b9e4f0d31257daf2
Ahmad <35385652+arezazadeh@users.noreply.github.com>
2022-03-05 20:14:33 -0800

    Update MyConfigParser.py

--- src/ConfigParser/MyConfigParser.py
+++ src/ConfigParser/MyConfigParser.py
@@ -7,29 +7,6 @@ class ParentObj:
 
 
 class ConfigParser:
-    """
-    - Example: Finding Routing Protocol
-    my_file = "stnhlv-21-sa01_running_config.txt"
-
-    parse = ConfigParser(my_file)
-
-    obj_list = parse.find_parent_child("^router")
-
-    for i in obj_list:
-        print(i.parent)
-        for child_obj in i.child:
-            print(child_obj)
-
-    - Finding Interface and Helper address Example
-
-    for i in obj_list:
-        vlan_200 = re.search("Vlan200", i.parent)
-        if vlan_200:
-            print(i.parent)
-            for c_obj in i.child:
-                if str(c_obj).startswith(" ip helper"):
-                    print(str(c_obj))
-    """
 
     def __init__(self, file):
         self.file = file
