be8faf38a78b36853e2d398ba530958a8d7e7a64
Ahmad Rezazadeh <ahmad1785@gmail.com>
2022-03-06 12:27:35 -0800

    svi obj added

--- src/ConfigParser/MyConfigParser.py
+++ src/ConfigParser/MyConfigParser.py
@@ -6,6 +6,18 @@ class ParentObj:
         self.child = child
 
 
+class IntObj:
+    def __init__(self, intf, ip_add, description, vrf, helper):
+        self.intf = intf
+        self.ip_add = ip_add
+        self.description = description
+        self.vrf = vrf
+        self.helper = helper
+
+    def __str__(self):
+        return f"IntObj Class - {self.intf}"
+
+
 class ConfigParser:
 
     def __init__(self, file):
@@ -32,5 +44,63 @@ class ConfigParser:
                         self.obj_list.append(ParentObj(parent, child))
 
         return self.obj_list
+    
+    
+    def get_svi_objects(self):
+    """
+    :param file: 
+    :return: list of "obj_list" where you can forloop over and use IntObj methods to access the values 
+    
+    :example:
+    file = "switch_01-run_config.txt"
+
+    obj = get_int_obj(file)
+    for i in obj:
+        if "lan200" in i.intf:
+            print(i.intf)
+            print(i.ip_add)
+            print(i.vrf)
+            print(i.description)
+            print(i.helper)
+    
+    """
+    intf_vlan_list = []
+
+    with open(file, "r") as f:
+        content = f.read()
+        split_on_bang = re.split("^!$", content, flags=re.MULTILINE)
+        for obj in split_on_bang:
+            parent_obj = re.findall("^interface Vlan(.*)", obj, flags=re.MULTILINE)
+            if parent_obj:
+                intf_vlan_list.append(obj)
+
+    obj_list = []
+    intf = ""
+    description = ""
+    ip_add = ""
+    vrf_member = ""
+    for i in intf_vlan_list:
+        helper_list = []
+        line = re.split("\n", i)
+        for ent in line:
+            if ent:
+                if ent.startswith("interface Vlan"):
+                    intf = ent
+                if ent.startswith(" description"):
+                    description = ent
+
+                if ent.startswith(" ip address"):
+                    ip_add = ent
+
+                if ent.startswith(" ip helper-address"):
+                    helper_list.append(ent)
+
+                if ent.startswith(" ip vrf for"):
+                    vrf_member = ent
+
+        intf_entity = IntObj(intf, ip_add, description, vrf_member, helper_list)
+        obj_list.append(intf_entity)
+
+    return obj_list
 
 
