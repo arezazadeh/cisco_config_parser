3e8c5c30413dee8b6175283fb8d46c7709bf4e37
Ahmad Rezazadeh <ahmad1785@gmail.com>
2022-03-06 22:30:02 -0800

    support for routed port added

--- src/cisco_config_parser/cisco_config_parser.py
+++ src/cisco_config_parser/cisco_config_parser.py
@@ -1,23 +1,6 @@
 import re
-
-
-class ParentObj:
-    def __init__(self, parent, child):
-        self.parent = parent
-        self.child = child
-
-
-class IntObj:
-    def __init__(self, intf, ip_add, description, vrf, helper, state):
-        self.intf = intf
-        self.ip_add = ip_add
-        self.description = description
-        self.vrf = vrf
-        self.helper = helper
-        self.state = state
-
-    def __str__(self):
-        return f"IntObj Class - {self.intf}"
+import ipaddress
+from obj import *
 
 
 class ConfigParser:
@@ -70,6 +53,136 @@ class ConfigParser:
                         self.obj_list_1.append(ParentObj(parent, child))
 
         return self.obj_list_1
+
+
+    def get_interface(self):
+        port_list = []
+
+        with open(self.file, "r") as f:
+            content = f.read()
+            split_on_bang = re.split("^!$", content, flags=re.MULTILINE)
+            for obj in split_on_bang:
+                parent_obj = re.findall("^interface\s+(.*)", obj, flags=re.MULTILINE)
+                if parent_obj:
+                    port_list.append(obj)
+
+        return port_list
+
+    def get_switchport(self):
+        obj_list = []
+        switchport_interface_list = []
+        port_list = self.get_interface()
+        port = ""
+        vlan = ""
+        voice = ""
+        description = ""
+        mode = ""
+
+        for i in port_list:
+            interface_parent = re.split("\n", i)
+            for line in interface_parent:
+                if "switchport" in line:
+                    switchport_interface_list.append(i)
+
+        for line in switchport_interface_list:
+            split_line = re.split("\n", line)
+            split_line.remove("")
+            split_line.remove("")
+            port = split_line[0]
+            for i in split_line:
+                if "description" in i:
+                    description = i
+
+                if "voice vlan" in i:
+                    voice_vlan_id = i.split("voice")[1]
+                    voice = f"Voice {voice_vlan_id}"
+
+                if "switchport access vlan" in i:
+                    vlan_id = i.split("access vlan")[1]
+                    vlan = f"Vlan {vlan_id}"
+
+                if "switchport access" in i:
+                    mode = "Access Port"
+                if "switchport trunk" in i:
+                    mode = "Trunk Port"
+
+            obj_list.append(SwitchPort(port, 
+                                       vlan=vlan, 
+                                       description=description, 
+                                       voice=voice, 
+                                       mode=mode
+                                       )
+                            )
+            port = ""
+            vlan = ""
+            voice = ""
+            description = ""
+            mode = ""
+
+        return obj_list
+
+    def get_routed_port(self):
+        obj_list = []
+        port_list = self.get_interface()
+        routed_port_list = []
+        for i in port_list:
+            interface_parent = re.split("\n", i)
+            for line in interface_parent:
+                if "ip address" in line:
+                    routed_port_list.append(i)
+        intf = ""
+        description = ""
+        ip_add = ""
+        mask = ""
+        subnet = ""
+        vrf_member = ""
+        state = ""
+
+        for line in routed_port_list:
+            helper_list = []
+            split_line = re.split("\n", line)
+            split_line.remove("")
+            split_line.remove("")
+            for ent in split_line:
+                if ent.startswith(" shutdown"):
+                    state = ent
+                if ent.startswith("interface "):
+                    intf = ent
+                if ent.startswith(" description"):
+                    description = ent
+                if ent.startswith(" ip address"):
+                    address = ent.split("ip address")[1]
+                    addr_mask = address.split()
+                    ip_add = addr_mask[0]
+                    mask = addr_mask[1]
+                    subnet = ipaddress.ip_network(f"{ip_add}/{mask}", strict=False)
+
+                if ent.startswith(" ip helper-address"):
+                    helper_list.append(ent)
+                if ent.startswith(" ip vrf for"):
+                    vrf_member = ent
+                if ent.startswith(" vrf member"):
+                    vrf_member = ent
+            if state == "":
+                state = " no shutdown"
+            obj_list.append(RoutedPort(intf, 
+                                       ip_add=ip_add, 
+                                       mask=mask, 
+                                       subnet=subnet, 
+                                       description=description, 
+                                       vrf_member=vrf_member, 
+                                       helper_list=helper_list, 
+                                       state=state
+                                       )
+                            )
+            intf = ""
+            description = ""
+            ip_add = ""
+            mask = ""
+            subnet = ""
+            vrf_member = ""
+            state = ""
+        return obj_list
 
     def get_svi_objects(self):
         """
---
+++ src/cisco_config_parser/obj.py
@@ -0,0 +1,45 @@
+class ParentObj:
+    def __init__(self, parent, child):
+        self.parent = parent
+        self.child = child
+
+
+class IntObj:
+    def __init__(self, intf, ip_add, description, vrf, helper, state):
+        self.intf = intf
+        self.ip_add = ip_add
+        self.description = description
+        self.vrf = vrf
+        self.helper = helper
+        self.state = state
+
+    def __str__(self):
+        return f"IntObj Class - {self.intf}"
+
+
+class SwitchPort:
+    def __init__(self, port, **kwargs):
+        self.port = port
+        self.vlan = kwargs.get("vlan") or None
+        self.voice = kwargs.get("voice") or None
+        self.description = kwargs.get("description") or None
+        self.mode = kwargs.get("mode") or None
+
+    def __str__(self):
+        return f"SwitchPort Class - {self.port}"
+
+
+class RoutedPort:
+    def __init__(self, intf, **kwargs):
+        self.intf = intf
+        self.ip_add = kwargs.get("ip_add") or None
+        self.mask = kwargs.get("mask") or None
+        self.subnet = kwargs.get("subnet") or None
+        self.description = kwargs.get("description") or None
+        self.vrf = kwargs.get("vrf") or None
+        self.helper = kwargs.get("helper") or None
+        self.state = kwargs.get("state") or None
+
+    def __str__(self):
+        return f"RoutedPort Class - {self.intf}"
+
