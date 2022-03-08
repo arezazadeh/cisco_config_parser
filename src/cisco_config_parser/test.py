f84385d154165b66056e2de9c7753df2993affed
Ahmad Rezazadeh <ahmad1785@gmail.com>
2022-03-08 10:07:48 -0800

    rebuilding

--- src/cisco_config_parser/test.py
+++
@@ -1,9 +0,0 @@
-
-from cisco_config_parser import ConfigParser
-file = "/Users/ahmad/running_config.txt"
-
-parser = ConfigParser(file=file)
-
-obj = parser.get_switchport()
-for i in obj:
-    print(i.port)
