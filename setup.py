57bc7aadee877d2aea310887e1136f53bfa1ee84
Ahmad Rezazade <ahmad1785@gmail.com>
2024-09-09 16:44:40 -0700

    version updated

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '2.1'
+VERSION = '2.2'
 PACKAGE_NAME = 'cisco_config_parser'
 AUTHOR = 'Ahmad Rezazadeh'
 AUTHOR_EMAIL = 'ahmad@klevernet.io'
