c652dfdc38e730810dfe19341818303ee3fb0617
Ahmad Rezazade <ahmad1785@gmail.com>
2024-09-15 22:46:07 -0700

    version updated

--- cisco_config_parser/__init__.py
+++ cisco_config_parser/__init__.py
@@ -6,7 +6,7 @@ _ALL_ = [
     "ConfigParserOld",
 ]
 
-__version__ = "1.4.6"
+__version__ = "2.2.5"
 __author__ = "Ahmad Rezazadeh"
 __email__ = "ahmad@klevernet.io"
 __url__ = "klevernet.ai"
--- cisco_config_parser/setup.py
+++ cisco_config_parser/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='cisco_config_parser',
-    version='2.0',
+    version='2.2.5',
     packages=find_packages(),
 )
